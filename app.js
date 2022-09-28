6aabaf8bd4b86c4791a890e840bd72c56096a8a9
Voterizes <114609574+Voterizes@users.noreply.github.com>
2022-09-28 20:37:34 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFF00";
+tg.MainButton.color = "#FF4500";
+
+let item = '0';
+
+
+let idAge = document.getElementById("idAge");
+let idfirstName = document.getElementById("idfirstName");
+let idsecondName = document.getElementById("idsecondName");
+
+
+
+
+
+document.querySelector("#idButton").onclick = () => {
+	let ageId = document.querySelector('#idAge').value;
+	let firstNameid = document.querySelector('#idfirstName').value;
+	let secondNameid = document.querySelector('#idsecondName').value;
+}
+
+
+
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	// tg.sendData(item); 
+	let media = [ ];
+		
+	media = document.getElementById("idAge");
+	media = document.getElementById("idfirstName");
+	media = document.getElementById("idsecondName");
+    tg.sendData(media); 
+});
+
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.innerText =`${tg.initDataUnsafe.first_name}
+	${tg.initDataUnsafe.last_name}`;
+	
+usercard.appendChild(p);
